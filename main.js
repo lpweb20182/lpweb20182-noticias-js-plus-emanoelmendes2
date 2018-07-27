b8b8bb86fcd3c6b6104fc46d2298dcd57e96572c
emanoelmendes2 <35959016+emanoelmendes2@users.noreply.github.com>
2018-07-27 17:18:32 -0300

    lpweb20182-noticias-js-plus-emanoelmendes2

--- main.js
+++ main.js
@@ -3,12 +3,31 @@ var noticias = [];
 function atualizarLista(noticia) {
     var lista = document.getElementById('noticias-recentes-list');
     var li = document.createElement('li');
+    var noticia_data = new Date(Date.parse(noticia.datah));
+    var data_atual = new Date().getTime();
+
     li.setAttribute('id', 'noticia-' + noticia.id);
     li.setAttribute('class', 'noticia');
-    li.innerHTML = '<p class="titulo" onclick="mostrarNoticia(' + noticia.id + ')">'
+    if (noticia_data <= data_atual){
+        li.innerHTML = '<p class="titulo" onclick="mostrarNoticia(' + noticia.id + ')">'
         + noticia.titulo
         + '</p>'
         + '<p class="conteudo">'
+        + '<br>'
+        + '<span>Nome:</span>'
+        + '<br>'
+        + noticia.nome
+        + '<br>'
+        + '<span>Email:</span>'
+        + '<br>'
+        + noticia.email
+        + '<br>'
+        + '<span>Data e Hora:</span>'
+        + '<br>'
+        + noticia.datah
+        + '<br>'
+        + '<span>Conteudo:</span>'
+        + '<br>'
         + noticia.conteudo
         + '<br>'
         + '<span>------------------</span>'
@@ -17,14 +36,22 @@ function atualizarLista(noticia) {
         + '</p>';
     lista.appendChild(li);
 }
+}
 
 function salvar(form) {
     var titulo = document.getElementById('frm-titulo').value;
+    var nome = document.getElementById('frm-nome').value;
+    var email = document.getElementById('frm-email').value;
+    var datah = document.getElementById('frm-datah').value;
     var conteudo = document.getElementById('frm-conteudo').value;
     var noticia = {
         id: noticias.length,
         titulo: titulo,
+        nome: nome,
+        email: email,
+        datah: datah,
         conteudo: conteudo
+        
     };
     noticias.push(noticia);
     atualizarLista(noticia);
